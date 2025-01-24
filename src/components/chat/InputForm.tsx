84103441a344e8f2d55981785cb77a7e9d9f3ac5
Louis Delmas <louis.delmas@outlook.com>
2025-01-24 13:00:05 +0100

    feat: 🎸 validate prompt input

--- src/components/chat/InputForm.tsx
+++ src/components/chat/InputForm.tsx
@@ -1,16 +1,41 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import clsx from "clsx";
 import { SendHorizonal } from "lucide-react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+const schema = z
+  .object({
+    prompt: z.string().nonempty(),
+  })
+  .required();
 
 export default function InputForm() {
+  const {
+    register,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+    mode: "onChange",
+  });
+
   return (
-    <div className="flex flex-row gap-4 items-center">
+    <form className="flex flex-row gap-4 items-center">
       <input
+        {...register("prompt")}
         className="flex-1 bg-brand-orange bg-opacity-20 p-4 rounded font-medium focus:outline-brand-orange"
         placeholder="Ask something"
       />
 
-      <SendHorizonal className="text-onbase-primary opacity-60" size={24} />
-    </div>
+      <SendHorizonal
+        className={clsx(
+          "text-onbase-primary",
+          errors && errors.prompt && "opacity-20",
+        )}
+        size={24}
+      />
+    </form>
   );
 }
