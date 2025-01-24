9baef2deced71836714acf42fb9971db6dff7860
Louis Delmas <louis.delmas@outlook.com>
2025-01-24 16:07:29 +0100

    feat: 🎸 scroll to bottom of the message container when streamin

--- src/components/chat/ChatView.tsx
+++ src/components/chat/ChatView.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect, useRef } from "react";
 import ExamplesView from "../examples/ExamplesView";
 import InputForm from "./InputForm";
 import MessagesView from "./MessagesView";
@@ -7,10 +8,21 @@ import useChat from "@/hooks/useChat";
 
 export default function ChatView() {
   const { messages, chatAsync, isStreaming, lastSystemMessageText } = useChat();
+  const messagesContainer = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    messagesContainer.current?.scrollTo({
+      top: messagesContainer.current.scrollHeight,
+      behavior: "smooth",
+    });
+  }, [isStreaming, lastSystemMessageText]);
 
   return (
     <div className="flex flex-col flex-1 overflow-auto gap-4">
-      <div className="flex flex-col flex-1 overflow-y-scroll gap-4 pr-4">
+      <div
+        className="flex flex-col flex-1 overflow-y-scroll gap-4 pr-4"
+        ref={messagesContainer}
+      >
         {messages.length ? (
           <MessagesView
             messages={messages}
